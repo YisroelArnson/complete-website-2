ed9e67f749f1068bc4551b58e2abd47f3277aad3
YisroelArnson <yisroelrnsn@gmail.com>
2016-06-20 15:29:10 -0400

    changed structure, and started on js for testimonials

---
+++ complete-website-2.js
@@ -0,0 +1,17 @@
+var $testimonialContainer = $('.testimonials-container');
+var $info1 = $testimonialContainer.find('.testimonial-info-1');
+var $info2 = $testimonialContainer.find('.testimonial-info-2');
+var $info3 = $testimonialContainer.find('.testimonial-info-3');
+var $leftArrow = $testimonialContainer.find('.change-testimonial.left');
+var $rightArrow = $testimonialContainer.find('.change-testimonial.right');
+
+
+// var $face = $testimonialContainer.find('.testimonial-face');
+// var $name = $testimonialContainer.find('.testimonial-name');
+// var $quote = $testimonialContainer.find('testimonial-quote');
+$leftArrow.on('click', changeTestimonial);
+$rightArrow.on('click', changeTestimonial);
+
+function changeTestimonial() {
+	
+}
