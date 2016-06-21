d7dedb8fd963afb16e7e53870ecaba8c91b1c28b
YisroelArnson <yisroelrnsn@gmail.com>
2016-06-20 23:44:35 -0400

    changed structure again. fixed testimonials animation, still a little buggy, any suggestions?

--- complete-website-2.js
+++ complete-website-2.js
@@ -1,17 +1,41 @@
-var $testimonialContainer = $('.testimonials-container');
+var $testimonialContainer = $('.testimonials-master-container');
 var $info1 = $testimonialContainer.find('.testimonial-info-1');
 var $info2 = $testimonialContainer.find('.testimonial-info-2');
 var $info3 = $testimonialContainer.find('.testimonial-info-3');
-var $leftArrow = $testimonialContainer.find('.change-testimonial.left');
-var $rightArrow = $testimonialContainer.find('.change-testimonial.right');
+var $circle = $testimonialContainer.find('.circle');
+var $leftCircle = $testimonialContainer.find('.left');
+var $middleCircle = $testimonialContainer.find('.middle');
+var $rightCircle = $testimonialContainer.find('.right');
+var counter = 1;
+
+$leftCircle.on('click', function() {
+	counter = 1;
+	$circle.css('background-color', '#fff');
+	$(this).css('background-color', '#555');
+	changeInfo($info1);
+});
+
+$middleCircle.on('click', function() {
+	counter = 2;
+	$circle.css('background-color', '#fff');
+	$(this).css('background-color', '#555');
+	changeInfo($info2);
+});
+
+$rightCircle.on('click', function() {
+	counter = 3;
+	$circle.css('background-color', '#fff');
+	$(this).css('background-color', '#555');
+	changeInfo($info3);
+});
 
 
-// var $face = $testimonialContainer.find('.testimonial-face');
-// var $name = $testimonialContainer.find('.testimonial-name');
-// var $quote = $testimonialContainer.find('testimonial-quote');
-$leftArrow.on('click', changeTestimonial);
-$rightArrow.on('click', changeTestimonial);
+function changeInfo(infoId) {
+		$info1.css('display', 'none');
+		$info2.css('display', 'none');
+		$info3.css('display', 'none');
 
-function changeTestimonial() {
-	
+		infoId.fadeIn(1000);
+ 		infoId.css('display', 'initial');
+ 		infoId.css('display', 'flex');
 }
