383c741dd0fba835030a10ff67a06147c81ff311
YisroelArnson <yisroelrnsn@gmail.com>
2016-06-21 15:54:05 -0400

    finished testimonial, one little bug, when you click the first time nothing happehns, then it works after that

--- complete-website-2.js
+++ complete-website-2.js
@@ -1,4 +1,5 @@
 var $testimonialContainer = $('.testimonials-master-container');
+var $testimonialInfo = $testimonialContainer.find('.testimonial-info')
 var $info1 = $testimonialContainer.find('.testimonial-info-1');
 var $info2 = $testimonialContainer.find('.testimonial-info-2');
 var $info3 = $testimonialContainer.find('.testimonial-info-3');
@@ -6,36 +7,32 @@ var $circle = $testimonialContainer.find('.circle');
 var $leftCircle = $testimonialContainer.find('.left');
 var $middleCircle = $testimonialContainer.find('.middle');
 var $rightCircle = $testimonialContainer.find('.right');
-var counter = 1;
 
-$leftCircle.on('click', function() {
-	counter = 1;
+$circle.on('click', function() {
 	$circle.css('background-color', '#fff');
 	$(this).css('background-color', '#555');
-	changeInfo($info1);
-});
 
-$middleCircle.on('click', function() {
-	counter = 2;
-	$circle.css('background-color', '#fff');
-	$(this).css('background-color', '#555');
-	changeInfo($info2);
-});
+	$leftCircle.on('click', function() {
+		changeInfo($info1);
+	})
 
-$rightCircle.on('click', function() {
-	counter = 3;
-	$circle.css('background-color', '#fff');
-	$(this).css('background-color', '#555');
-	changeInfo($info3);
+	$middleCircle.on('click', function() {
+		changeInfo($info2);
+	})
+
+	$rightCircle.on('click', function() {
+		changeInfo($info3);
+	})
+
 });
 
 
-function changeInfo(infoId) {
-		$info1.css('display', 'none');
-		$info2.css('display', 'none');
-		$info3.css('display', 'none');
 
-		infoId.fadeIn(1000);
- 		infoId.css('display', 'initial');
- 		infoId.css('display', 'flex');
+
+function changeInfo(whichId) {
+		console.log(whichId)
+		$testimonialInfo.css('display', 'none');
+
+ 		whichId.css('display', 'initial');
+ 		whichId.css('display', 'flex');
 }
