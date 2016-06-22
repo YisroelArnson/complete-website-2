81439105f0e72aa0b65aa2b323122e69e0f2d488
YisroelArnson <yisroelrnsn@gmail.com>
2016-06-22 01:02:00 -0400

    changed some css for the footer

--- complete-website-2.js
+++ complete-website-2.js
@@ -1,38 +1,41 @@
-var $testimonialContainer = $('.testimonials-master-container');
-var $testimonialInfo = $testimonialContainer.find('.testimonial-info')
-var $info1 = $testimonialContainer.find('.testimonial-info-1');
-var $info2 = $testimonialContainer.find('.testimonial-info-2');
-var $info3 = $testimonialContainer.find('.testimonial-info-3');
-var $circle = $testimonialContainer.find('.circle');
-var $leftCircle = $testimonialContainer.find('.left');
-var $middleCircle = $testimonialContainer.find('.middle');
-var $rightCircle = $testimonialContainer.find('.right');
+$(document).ready(function() {
+	var $testimonialContainer = $('.testimonials-master-container');
+	var $testimonialInfo = $testimonialContainer.find('.testimonial-info')
+	var $info1 = $testimonialContainer.find('.testimonial-info-1');
+	var $info2 = $testimonialContainer.find('.testimonial-info-2');
+	var $info3 = $testimonialContainer.find('.testimonial-info-3');
+	var $circle = $testimonialContainer.find('.circle');
+	var $leftCircle = $testimonialContainer.find('.left');
+	var $middleCircle = $testimonialContainer.find('.middle');
+	var $rightCircle = $testimonialContainer.find('.right');
 
-$circle.on('click', function() {
-	$circle.css('background-color', '#fff');
-	$(this).css('background-color', '#555');
+	$circle.on('click', function() {
+		$circle.css('background-color', '#fff');
+		$(this).css('background-color', '#555');
 
-	$leftCircle.on('click', function() {
-		changeInfo($info1);
-	})
+		$leftCircle.on('click', function() {
+			changeInfo($info1);
+		})
 
-	$middleCircle.on('click', function() {
-		changeInfo($info2);
-	})
+		$middleCircle.on('click', function() {
+			changeInfo($info2);
+		})
 
-	$rightCircle.on('click', function() {
-		changeInfo($info3);
-	})
+		$rightCircle.on('click', function() {
+			changeInfo($info3);
+		})
+
+	});
+
+
+
+
+	function changeInfo(whichId) {
+			console.log(whichId)
+			$testimonialInfo.css('display', 'none');
+
+	 		whichId.css('display', 'initial');
+	 		whichId.css('display', 'flex');
+	}
 
 });
-
-
-
-
-function changeInfo(whichId) {
-		console.log(whichId)
-		$testimonialInfo.css('display', 'none');
-
- 		whichId.css('display', 'initial');
- 		whichId.css('display', 'flex');
-}
