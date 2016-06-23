038115d19ccae1164f2dcc162e30afdb22eb9002
YisroelArnson <yisroelrnsn@gmail.com>
2016-06-23 15:18:40 -0400

    cleaned up code

--- complete-website-2.js
+++ complete-website-2.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	var $testimonialContainer = $('.testimonials-master-container');
-	var $testimonialInfo = $testimonialContainer.find('.testimonial-info')
-	var $info1 = $testimonialContainer.find('.testimonial-info-1');
-	var $info2 = $testimonialContainer.find('.testimonial-info-2');
-	var $info3 = $testimonialContainer.find('.testimonial-info-3');
-	var $circle = $testimonialContainer.find('.circle');
-	var $leftCircle = $testimonialContainer.find('.left');
-	var $middleCircle = $testimonialContainer.find('.middle');
-	var $rightCircle = $testimonialContainer.find('.right');
-
 	checkTestimonial();
 	clearContact();
 	smoothScroll(800);
 
 	function checkTestimonial () {
+		var $testimonialContainer = $('.testimonials-master-container');
+		var $testimonialInfo = $testimonialContainer.find('.testimonial-info')
+		var $info1 = $testimonialContainer.find('.testimonial-info-1');
+		var $info2 = $testimonialContainer.find('.testimonial-info-2');
+		var $info3 = $testimonialContainer.find('.testimonial-info-3');
+		var $circle = $testimonialContainer.find('.circle');
+		var $leftCircle = $testimonialContainer.find('.left');
+		var $middleCircle = $testimonialContainer.find('.middle');
+		var $rightCircle = $testimonialContainer.find('.right');
+
 		$circle.on('click', function() {
 			$circle.css('background-color', '#fff');
 			$(this).css('background-color', '#555');
@@ -39,33 +39,40 @@ $(document).ready(function() {
 		}
 	}
 
-	function clearContact() {
-	var	$inputContainer = $('.contact-input-container');
-	var $inputs =  $inputContainer.find('input');
 
-	$inputs.on('focus', function() {
-		var $this = $(this);
-		$this.val('');
-		$this.keyup(function() {
-			var value = $this.val();
-			$this.val(value);
+	function clearContact() {
+		var	$inputContainer = $('.contact-input-container');
+		var $inputs =  $inputContainer.find('input');
+
+		$inputs.on('focus', function() {
+			var $this = $(this);
+			$this.val('');
+			$this.keyup(function() {
+				var value = $this.val();
+				$this.val(value);
+			});
+
+			$this.on('blur', function() {
+					if($this.val() === '') {
+						$this.val($this.attr('name'));
+					}
+			});
 		});
-	});
 	}
 
 
 	function smoothScroll (duration) {
-	$('a[href^="#"]').on('click', function(event) {
+		$('a[href^="#"]').on('click', function(event) {
 
-	    var target = $( $(this).attr('href') );
+		    var target = $( $(this).attr('href') );
 
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top
-	        }, duration);
-	    }
-	});
-	}
+		    if( target.length ) {
+		        event.preventDefault();
+		        $('html, body').animate({
+		            scrollTop: target.offset().top
+		        }, duration);
+		    }
+		});
+		}
 
 });	
